fbad37fc74abd6a3b44f36603c15a6501db2466d
yulya <yliuakomissarova@gmail.com>
2023-03-16 18:56:06 +0500

    Interface done

--- app/src/main/java/com/example/habitstracker/MainActivity.kt
+++ app/src/main/java/com/example/habitstracker/MainActivity.kt
@@ -1,20 +1,59 @@
 package com.example.habitstracker
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.example.habitstracker.databinding.ActivityMainBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HabitAdapter.OnHabitCardListener {
+
+    private var noHabitsMessage: TextView? = null
+
+    companion object {
+        var habits: MutableList<Habit> = mutableListOf()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noHabitsMessage = findViewById(R.id.no_habits_message)
+
+        setListenerOnAddHabitButton()
+    }
+
+    private fun setListenerOnAddHabitButton() {
+        val addHabitButton: FloatingActionButton = findViewById(R.id.add_habit_button)
+        addHabitButton.setOnClickListener {
+            val intent = Intent(this, HabitEditingActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        noHabitsMessage?.visibility = if (habits.size == 0) View.VISIBLE else View.GONE
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val recyclerView: RecyclerView = findViewById(R.id.habits_list)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = HabitAdapter(habits, applicationContext, this)
+    }
+
+    override fun onHabitCardClick(position: Int) {
+        val intent = Intent(this, HabitEditingActivity::class.java)
+            .apply {
+                putExtra(getString(R.string.intent_extra_habit_position), position)
+                putExtra(getString(R.string.intent_extra_habit), habits[position]) }
+        startActivity(intent)
     }
 }
